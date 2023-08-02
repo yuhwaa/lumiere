e4ff463792a09090949e3203e57be4e2781df2fe
Yu <yuhwachn@gmail.com>
2023-08-01 21:37:10 -0400

    finished navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom"
 
 export function Navbar() {
     return (
-    <NavbarBS className="bg-white shadow-sm mb-3">
+    <NavbarBS sticky="top" className="bg-white shadow-sm mb-3">
         <Container>
             <Nav className="me-auto">
                 <Nav.Link to='/' as ={NavLink}>
