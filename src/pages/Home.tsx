04c17ec8a2acab5bb69f23e32194e9476a3ea3bc
Yu <yuhwachn@gmail.com>
2023-10-30 22:48:23 -0400

    add notes for home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,13 @@ import { CarouselComponent } from "../components/CarouselComponent";
 export function Home(){
     return (
     <>
+    {/* Carousel */}
     <h1 className="text-center">Welcome to Lumière Pâtisserie</h1>
       <CarouselComponent data={data} />
+    {/* Menu */}
+    {/* Reservation */}
+    {/* Membership */}
+    {/* Footer: location/phone/email + Social + hours + newsletter + all rights reserved*/}
     </>
     )
 }
