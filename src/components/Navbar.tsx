6fdceaa81d2e695879354f048b338003c80d868f
Yu <yuhwachn@gmail.com>
2023-11-08 22:47:43 -0500

    updated navbar and footer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,55 +1,55 @@
-import { Button, Container, Nav, Navbar as NavbarBS} from "react-bootstrap" 
-import { NavLink } from "react-router-dom"
-import { useShoppingCart } from "../context/ShoppingCartContext"
-
+import { useState, useEffect } from "react";
+import { Button, Container, Nav, Navbar as NavbarBS } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
 export function Navbar() {
-    const {openCart, cartQuantity} = useShoppingCart()
-    return (
-    <NavbarBS sticky="top" className="bg-white shadow-sm mb-3">
-        <Container>
-            <Nav className="me-auto">
-                <Nav.Link to='/' as ={NavLink}>
-                    Home
-                </Nav.Link>
-                <Nav.Link to='/store' as ={NavLink}>
-                    Store
-                </Nav.Link>
-                <Nav.Link to='/about' as ={NavLink}>
-                    About
-                </Nav.Link>
-                </Nav>
+  const { openCart, cartQuantity } = useShoppingCart();
+  const [scrolled, setScrolled] = useState(false);
+  //Hook: It allows you to perform actions in response to component lifecycle events, such as component mounting, updating, and unmounting.
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    //cleanup function
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const navbarClasses = `navbar ${scrolled ? "bg-white shadow-sm" : "transparent"}`;
+
+  return (
+    <NavbarBS sticky="top" className={navbarClasses}>
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
         {cartQuantity > 0 && (
-        <Button 
+          <Button
             onClick={openCart}
             style={{ width: "3rem", height: "3rem", position: "relative" }}
             variant="outline-primary"
             className="rounded-circle"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 576 512"
-              fill="currentColor"
-            >
-              <path d="M96 0C107.5 0 117.4 8.19 119.6 19.51L121.1 32H541.8C562.1 32 578.3 52.25 572.6 72.66L518.6 264.7C514.7 278.5 502.1 288 487.8 288H170.7L179.9 336H488C501.3 336 512 346.7 512 360C512 373.3 501.3 384 488 384H159.1C148.5 384 138.6 375.8 136.4 364.5L76.14 48H24C10.75 48 0 37.25 0 24C0 10.75 10.75 0 24 0H96zM128 464C128 437.5 149.5 416 176 416C202.5 416 224 437.5 224 464C224 490.5 202.5 512 176 512C149.5 512 128 490.5 128 464zM512 464C512 490.5 490.5 512 464 512C437.5 512 416 490.5 416 464C416 437.5 437.5 416 464 416C490.5 416 512 437.5 512 464z" />
-            </svg>
-            
-            <div
-              className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
-              style={{
-                color: "white",
-                width: "1.5rem",
-                height: "1.5rem",
-                position: "absolute",
-                bottom: 0,
-                right: 0,
-                transform: "translate(25%, 25%)",
-              }}
-            >
-                {cartQuantity}
-            </div>
-          </Button>)}
+            {/* Your cart icon */}
+          </Button>
+        )}
       </Container>
     </NavbarBS>
-  )
+  );
 }
